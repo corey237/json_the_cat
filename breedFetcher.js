5ddfdc4ac97521ad2a3aaf632aea80381993014d
Corey Silver <coreysilver@outlook.com>
2023-01-19 17:29:04 -0500

    Refactored the breedFetcher assignment.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,24 @@
 const request = require("request");
-const breed = process.argv[2];
-request(`https://api.thecatapi.com/v1/breeds/search/?name=${breed}`, (error, response, body) => {
-  if (error) {
-    console.log("An error has occurred, please try again.");
-  } else if (!response) {
-    console.log("An error has occurred with the response", response);
-  } else {
-    const data = JSON.parse(body);
-    console.log(data[0].description);
-  }
-});
+
+
+const fetchBreedDescription = function(breed, callback) {
+
+  request(`https://api.thecatapi.com/v1/breeds/search/?name=${breed}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      console.log("An error has occurred, please try again.");
+    } else if (!response) {
+      console.log("An error has occurred with the response", response);
+    } else {
+      const data = JSON.parse(body);
+      callback(null, data[0].description);
+    }
+  });
+
+};
+
+
+module.exports = { fetchBreedDescription };
 
 
 
---
+++ index.js
@@ -0,0 +1,11 @@
+const { fetchBreedDescription } = require('./breedFetcher')
+
+const breed = process.argv[2];
+
+fetchBreedDescription(breed, (error, desc) => {
+    if(error) {
+        console.log(error)
+    } else {
+        console.log(desc)
+    }
+})
